6560dbb89b08c8821b9fa5affa7a249952900a39
Stephen Hemminger <shemminger@linux-foundation.org>
2007-06-20 10:55:18 -0700

    fix build warnings
    Fix problems from recent if.h related changes.
    
    Signed-off-by: Stephen Hemminger <shemminger@linux-foundation.org>

--- include/iptables_common.h
+++ include/iptables_common.h
@@ -12,7 +12,7 @@ enum exittype {
 /* this is a special 64bit data type that is 8-byte aligned */
 #define aligned_u64 unsigned long long __attribute__((aligned(8)))
 
-extern void exit_printhelp() __attribute__((noreturn));
+extern void exit_printhelp(void) __attribute__((noreturn));
 extern void exit_tryhelp(int) __attribute__((noreturn));
 int check_inverse(const char option[], int *invert, int *optind, int argc);
 extern int string_to_number(const char *, 
