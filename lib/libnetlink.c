6e46ec813b7f06a85acb5a348dfc1a25ea488024
Williams, Mitch A <mitch.a.williams@intel.com>
2010-02-10 01:46:47 +0000

    libnetlink: Modify the parser to track first duplicated attributes
    Modify the parser to keep track of the first of any duplicated attributes,
    instead of the last. This is required for VF configuration reporting, where
    multiple attributes of the same type are added sequentially.
    
    Signed-off-by: Mitch Williams <mitch.a.williams@intel.com>
    Signed-off-by: Jeff Kirsher <jeffrey.t.kirsher@intel.com>

--- lib/libnetlink.c
+++ lib/libnetlink.c
@@ -644,7 +644,7 @@ int parse_rtattr(struct rtattr *tb[], int max, struct rtattr *rta, int len)
 {
 	memset(tb, 0, sizeof(struct rtattr *) * (max + 1));
 	while (RTA_OK(rta, len)) {
-		if (rta->rta_type <= max)
+		if ((rta->rta_type <= max) && (!tb[rta->rta_type]))
 			tb[rta->rta_type] = rta;
 		rta = RTA_NEXT(rta,len);
 	}
