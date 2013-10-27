iproute2/iproute2

commit e9e78b0db0e023035e346ba67de838be851eb665

tc: allow qdisc without options
Pfifo_fast needs no options. So don't force it to have parsing code.

tc/tc_qdisc.c
@@ -137,15 +137,15 @@ static int tc_qdisc_modify(int cmd, unsigned flags, int argc, char **argv)
 	if (est.ewma_log)
 		addattr_l(&req.n, sizeof(req), TCA_RATE, &est, sizeof(est));
 
-	if (q) {
-		if (!q->parse_qopt) {
-			fprintf(stderr, "qdisc '%s' does not support option parsing\n", k);
-			return -1;
-		}
-		if (q->parse_qopt(q, argc, argv, &req.n))
-			return 1;
-	} else {
-		if (argc) {
+	if (argc) {
+		if (q) {
+			if (!q->parse_qopt) {
+				fprintf(stderr, "qdisc '%s' does not support option parsing\n", k);
+				return -1;
+			}
+			if (q->parse_qopt(q, argc, argv, &req.n))
+				return 1;
+		} else {
 			if (matches(*argv, "help") == 0)
 				usage();
 

commit 0a502b21e30be835dcad8d9c6023a41da8709eb1

Fix handling of qdis without options
Some qdisc like htb want the parse_qopt to be called even if no options
present. Fixes regression caused by:

e9e78b0db0e023035e346ba67de838be851eb665 is the first bad commit
commit e9e78b0db0e023035e346ba67de838be851eb665
Author: Stephen Hemminger <stephen@networkplumber.org>
Date:   Mon Aug 26 08:41:19 2013 -0700

    tc: allow qdisc without options

tc/tc_qdisc.c
@@ -137,15 +137,16 @@ static int tc_qdisc_modify(int cmd, unsigned flags, int argc, char **argv)
 	if (est.ewma_log)
 		addattr_l(&req.n, sizeof(req), TCA_RATE, &est, sizeof(est));
 
-	if (argc) {
-		if (q) {
-			if (!q->parse_qopt) {
-				fprintf(stderr, "qdisc '%s' does not support option parsing\n", k);
-				return -1;
-			}
+	if (q) {
+		if (q->parse_qopt) {
 			if (q->parse_qopt(q, argc, argv, &req.n))
 				return 1;
-		} else {
+		} else if (argc) {
+			fprintf(stderr, "qdisc '%s' does not support option parsing\n", k);
+			return -1;
+		}
+	} else {
+		if (argc) {
 			if (matches(*argv, "help") == 0)
 				usage();
 