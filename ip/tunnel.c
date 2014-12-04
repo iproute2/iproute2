14e976733077f02a2e2d5c5508c5795c7a5fbf3b
Stephen Hemminger <shemming@brocade.com>
2014-12-03 19:08:41 -0800

    tunnel: decode ESP tunnel type
    Add ESP to decode switch.

--- ip/tunnel.c
+++ ip/tunnel.c
@@ -51,6 +51,9 @@ const char *tnl_strproto(__u8 proto)
 	case IPPROTO_IPV6:
 		strcpy(buf, "ipv6");
 		break;
+	case IPPROTO_ESP:
+		strcpy(buf, "esp");
+		break;
 	case 0:
 		strcpy(buf, "any");
 		break;
