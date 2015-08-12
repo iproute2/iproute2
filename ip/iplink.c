d8cf93de04d9372f478fc2e59e2fe8bce1953b7a
Zhang Shengju <zhangshengju@cmss.chinamobile.com>
2015-08-12 06:03:22 +0000

    iplink: add missing link type
    Add missing link type "bridge_slave".
    
    Signed-off-by: Zhang Shengju <zhangshengju@cmss.chinamobile.com>

--- ip/iplink.c
+++ ip/iplink.c
@@ -93,7 +93,7 @@ void iplink_usage(void)
 		fprintf(stderr, "TYPE := { vlan | veth | vcan | dummy | ifb | macvlan | macvtap |\n");
 		fprintf(stderr, "          bridge | bond | ipoib | ip6tnl | ipip | sit | vxlan |\n");
 		fprintf(stderr, "          gre | gretap | ip6gre | ip6gretap | vti | nlmon |\n");
-		fprintf(stderr, "          bond_slave | ipvlan | geneve }\n");
+		fprintf(stderr, "          bond_slave | ipvlan | geneve | bridge_slave }\n");
 	}
 	exit(-1);
 }
