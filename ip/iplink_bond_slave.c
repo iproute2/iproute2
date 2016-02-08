e6c38e2c59c922229ca615b6f913523dd6ee7c24
Nikolay Aleksandrov <nikolay@cumulusnetworks.com>
2016-02-08 17:13:58 +0100

    iplink: bond_slave: fix ad_actor/partner_oper_port_state output
    It seems that I've made a mistake when I exported these, instead of a
    space in the end I've put a newline character which is wrong and breaks
    the single line output.
    
    Fixes: 7d6bc3b87abad ("bonding: export 3ad actor and partner port state")
    Reported-by: Sam Tannous <stannous@cumulusnetworks.com>
    Signed-off-by: Nikolay Aleksandrov <nikolay@cumulusnetworks.com>

--- ip/iplink_bond_slave.c
+++ ip/iplink_bond_slave.c
@@ -80,11 +80,11 @@ static void bond_slave_print_opt(struct link_util *lu, FILE *f, struct rtattr *t
 			rta_getattr_u16(tb[IFLA_BOND_SLAVE_AD_AGGREGATOR_ID]));
 
 	if (tb[IFLA_BOND_SLAVE_AD_ACTOR_OPER_PORT_STATE])
-		fprintf(f, "ad_actor_oper_port_state %d\n",
+		fprintf(f, "ad_actor_oper_port_state %d ",
 			rta_getattr_u8(tb[IFLA_BOND_SLAVE_AD_ACTOR_OPER_PORT_STATE]));
 
 	if (tb[IFLA_BOND_SLAVE_AD_PARTNER_OPER_PORT_STATE])
-		fprintf(f, "ad_partner_oper_port_state %d\n",
+		fprintf(f, "ad_partner_oper_port_state %d ",
 			rta_getattr_u16(tb[IFLA_BOND_SLAVE_AD_PARTNER_OPER_PORT_STATE]));
 }
 
