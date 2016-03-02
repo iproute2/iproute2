7a53aa592fffbe5984aa8adbe76f5b370b247f05
Phil Sutter <phil@nwl.cc>
2016-03-02 19:19:49 +0100

    ip: align help text with manpage
    Although the ip command accepts both "neighbor" and "neighbour" as
    subcommand, I assume it's sufficient to list it in help text as just
    "neigh" like ip.8 does.
    
    Signed-off-by: Phil Sutter <phil@nwl.cc>

--- ip/ip.c
+++ ip/ip.c
@@ -49,7 +49,7 @@ static void usage(void)
 	fprintf(stderr,
 "Usage: ip [ OPTIONS ] OBJECT { COMMAND | help }\n"
 "       ip [ -force ] -batch filename\n"
-"where  OBJECT := { link | address | addrlabel | route | rule | neighbor | ntable |\n"
+"where  OBJECT := { link | address | addrlabel | route | rule | neigh | ntable |\n"
 "                   tunnel | tuntap | maddress | mroute | mrule | monitor | xfrm |\n"
 "                   netns | l2tp | fou | tcp_metrics | token | netconf }\n"
 "       OPTIONS := { -V[ersion] | -s[tatistics] | -d[etails] | -r[esolve] |\n"
