f1239ca1f96c76fbc0742ca0d0c7e87b9b15d437
Jiri Pirko <jiri@mellanox.com>
2016-04-15 09:51:44 +0200

    list: add list_for_each_entry_reverse macro
    Signed-off-by: Jiri Pirko <jiri@mellanox.com>

--- include/list.h
+++ include/list.h
@@ -50,8 +50,14 @@ static inline void list_del(struct list_head *entry)
 #define list_first_entry(ptr, type, member) \
 	list_entry((ptr)->next, type, member)
 
+#define list_last_entry(ptr, type, member) \
+	list_entry((ptr)->prev, type, member)
+
 #define list_next_entry(pos, member) \
 	list_entry((pos)->member.next, typeof(*(pos)), member)
+
+#define list_prev_entry(pos, member) \
+	list_entry((pos)->member.prev, typeof(*(pos)), member)
 
 #define list_for_each_entry(pos, head, member)				\
 	for (pos = list_first_entry(head, typeof(*pos), member);	\
@@ -63,6 +69,11 @@ static inline void list_del(struct list_head *entry)
 		n = list_next_entry(pos, member);			\
 	     &pos->member != (head);					\
 	     pos = n, n = list_next_entry(n, member))
+
+#define list_for_each_entry_reverse(pos, head, member)			\
+	for (pos = list_last_entry(head, typeof(*pos), member);		\
+	     &pos->member != (head);					\
+	     pos = list_prev_entry(pos, member))
 
 struct hlist_head {
 	struct hlist_node *first;
