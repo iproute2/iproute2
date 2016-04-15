ebaf76b55ea509bcfb8845e4c40a9ecd3e8377f5
Jiri Pirko <jiri@mellanox.com>
2016-04-15 09:51:45 +0200

    list: add list_add_tail helper
    Signed-off-by: Jiri Pirko <jiri@mellanox.com>

--- include/list.h
+++ include/list.h
@@ -33,6 +33,11 @@ static inline void list_add(struct list_head *new, struct list_head *head)
 	__list_add(new, head, head->next);
 }
 
+static inline void list_add_tail(struct list_head *new, struct list_head *head)
+{
+	__list_add(new, head->prev, head);
+}
+
 static inline void __list_del(struct list_head *prev, struct list_head *next)
 {
 	next->prev = prev;
