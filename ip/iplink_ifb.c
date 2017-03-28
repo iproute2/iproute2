9fd7b86c2de91935ab736ef6df119a0c691cdfdc
Phil Sutter <phil@nwl.cc>
2017-03-28 23:19:39 +0200

    ip: link: Add missing link type help texts
    These are basically stubs: The types which lacked their own help text
    simply don't accept any options (yet). Still it might be a bit confusing
    to users if they are presented with the generic 'ip link' help text
    instead of something saying there are no type specific options.
    
    Signed-off-by: Phil Sutter <phil@nwl.cc>

---
+++ ip/iplink_ifb.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "utils.h"
+#include "ip_common.h"
+
+static void ifb_print_help(struct link_util *lu,
+			    int argc, char **argv, FILE *f)
+{
+	fprintf(f, "Usage: ... ifb\n");
+}
+
+struct link_util ifb_link_util = {
+	.id		= "ifb",
+	.print_help	= ifb_print_help,
+};
---
+++ ip/iplink_nlmon.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "utils.h"
+#include "ip_common.h"
+
+static void nlmon_print_help(struct link_util *lu,
+			    int argc, char **argv, FILE *f)
+{
+	fprintf(f, "Usage: ... nlmon\n");
+}
+
+struct link_util nlmon_link_util = {
+	.id		= "nlmon",
+	.print_help	= nlmon_print_help,
+};
