ba914908eba9a84b028cdb0f679f39786e22dda7
Stephen Hemminger <stephen@networkplumber.org>
2017-11-07 11:02:41 +0900

    Update kernel headers with new SPDK identifier
    The kernel header sanitizisation process now puts SPDK GPLv2
    license comment on files.
    
    Signed-off-by: Stephen Hemminger <stephen@networkplumber.org>

--- include/uapi/linux/ila.h
+++ include/uapi/linux/ila.h
@@ -1,3 +1,4 @@
+/* SPDX-License-Identifier: GPL-2.0 WITH Linux-syscall-note */
 /* ila.h - ILA Interface */
 
 #ifndef _LINUX_ILA_H
--- include/uapi/rdma/rdma_netlink.h
+++ include/uapi/rdma/rdma_netlink.h
@@ -1,3 +1,4 @@
+/* SPDX-License-Identifier: GPL-2.0 WITH Linux-syscall-note */
 #ifndef _RDMA_NETLINK_H
 #define _RDMA_NETLINK_H
 
