cf7fe23859cd53740febce137d29f080efefd64c
Stephen Hemminger <stephen@networkplumber.org>
2018-08-20 16:00:38 -0700

    bridge: drop extern from function prototypes
    Signed-off-by: Stephen Hemminger <stephen@networkplumber.org>

--- bridge/br_common.h
+++ bridge/br_common.h
@@ -6,20 +6,20 @@
 #define MDB_RTR_RTA(r) \
 		((struct rtattr *)(((char *)(r)) + RTA_ALIGN(sizeof(__u32))))
 
-extern void print_vlan_info(FILE *fp, struct rtattr *tb);
-extern int print_linkinfo(const struct sockaddr_nl *who,
+void print_vlan_info(FILE *fp, struct rtattr *tb);
+int print_linkinfo(const struct sockaddr_nl *who,
 			  struct nlmsghdr *n,
 			  void *arg);
-extern int print_fdb(const struct sockaddr_nl *who,
+int print_fdb(const struct sockaddr_nl *who,
 		     struct nlmsghdr *n, void *arg);
-extern int print_mdb(const struct sockaddr_nl *who,
+int print_mdb(const struct sockaddr_nl *who,
 		     struct nlmsghdr *n, void *arg);
 
-extern int do_fdb(int argc, char **argv);
-extern int do_mdb(int argc, char **argv);
-extern int do_monitor(int argc, char **argv);
-extern int do_vlan(int argc, char **argv);
-extern int do_link(int argc, char **argv);
+int do_fdb(int argc, char **argv);
+int do_mdb(int argc, char **argv);
+int do_monitor(int argc, char **argv);
+int do_vlan(int argc, char **argv);
+int do_link(int argc, char **argv);
 
 extern int preferred_family;
 extern int show_stats;
