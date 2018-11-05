iproute2/iproute2

commit 508f3c231efb179fb842d222e8151b395937b136

Use libbsd for strlcpy if available
If libc does not provide strlcpy check for libbsd with pkg-config to
avoid relying on inline version.

Signed-off-by: Luca Boccassi <bluca@debian.org>
Signed-off-by: Stephen Hemminger <stephen@networkplumber.org>

genl/ctrl.c
@@ -18,6 +18,9 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <string.h>
+#ifdef HAVE_LIBBSD
+#include <bsd/string.h>
+#endif
 
 #include "utils.h"
 #include "genl_utils.h"

ip/iplink.c
@@ -24,6 +24,9 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <string.h>
+#ifdef HAVE_LIBBSD
+#include <bsd/string.h>
+#endif
 #include <sys/ioctl.h>
 #include <stdbool.h>
 #include <linux/mpls.h>

ip/ipnetns.c
@@ -8,6 +8,9 @@
 #include <sys/syscall.h>
 #include <stdio.h>
 #include <string.h>
+#ifdef HAVE_LIBBSD
+#include <bsd/string.h>
+#endif
 #include <sched.h>
 #include <fcntl.h>
 #include <dirent.h>

ip/iproute_lwtunnel.c
@@ -16,6 +16,9 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <string.h>
+#ifdef HAVE_LIBBSD
+#include <bsd/string.h>
+#endif
 #include <linux/ila.h>
 #include <linux/lwtunnel.h>
 #include <linux/mpls_iptunnel.h>

ip/ipvrf.c
@@ -21,6 +21,9 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#ifdef HAVE_LIBBSD
+#include <bsd/string.h>
+#endif
 #include <dirent.h>
 #include <errno.h>
 #include <limits.h>

ip/ipxfrm.c
@@ -28,6 +28,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#ifdef HAVE_LIBBSD
+#include <bsd/string.h>
+#endif
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <time.h>

ip/tunnel.c
@@ -24,6 +24,9 @@
 
 #include <stdio.h>
 #include <string.h>
+#ifdef HAVE_LIBBSD
+#include <bsd/string.h>
+#endif
 #include <unistd.h>
 #include <errno.h>
 #include <sys/types.h>

ip/xfrm_state.c
@@ -27,6 +27,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#ifdef HAVE_LIBBSD
+#include <bsd/string.h>
+#endif
 #include <netdb.h>
 #include "utils.h"
 #include "xfrm.h"

commit 6d2fd4a53f63bd20667b1a8f2ec8fde1fc3a54d4

Include bsd/string.h only in include/utils.h
This is simpler and cleaner, and avoids having to include the header
from every file where the functions are used. The prototypes of the
internal implementation are in this header, so utils.h will have to be
included anyway for those.

Fixes: 508f3c231efb ("Use libbsd for strlcpy if available")

Signed-off-by: Luca Boccassi <bluca@debian.org>
Signed-off-by: Stephen Hemminger <stephen@networkplumber.org>

genl/ctrl.c
@@ -18,9 +18,6 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <string.h>
-#ifdef HAVE_LIBBSD
-#include <bsd/string.h>
-#endif
 
 #include "utils.h"
 #include "genl_utils.h"

include/utils.h
@@ -9,6 +9,10 @@
 #include <stdbool.h>
 #include <time.h>
 
+#ifdef HAVE_LIBBSD
+#include <bsd/string.h>
+#endif
+
 #include "libnetlink.h"
 #include "ll_map.h"
 #include "rtm_map.h"

ip/iplink.c
@@ -24,9 +24,6 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <string.h>
-#ifdef HAVE_LIBBSD
-#include <bsd/string.h>
-#endif
 #include <sys/ioctl.h>
 #include <stdbool.h>
 #include <linux/mpls.h>

ip/ipnetns.c
@@ -8,9 +8,6 @@
 #include <sys/syscall.h>
 #include <stdio.h>
 #include <string.h>
-#ifdef HAVE_LIBBSD
-#include <bsd/string.h>
-#endif
 #include <sched.h>
 #include <fcntl.h>
 #include <dirent.h>

ip/iproute_lwtunnel.c
@@ -16,9 +16,6 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <string.h>
-#ifdef HAVE_LIBBSD
-#include <bsd/string.h>
-#endif
 #include <linux/ila.h>
 #include <linux/lwtunnel.h>
 #include <linux/mpls_iptunnel.h>

ip/ipvrf.c
@@ -21,9 +21,6 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
-#ifdef HAVE_LIBBSD
-#include <bsd/string.h>
-#endif
 #include <dirent.h>
 #include <errno.h>
 #include <limits.h>

ip/ipxfrm.c
@@ -28,9 +28,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#ifdef HAVE_LIBBSD
-#include <bsd/string.h>
-#endif
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <time.h>

ip/tunnel.c
@@ -24,9 +24,6 @@
 
 #include <stdio.h>
 #include <string.h>
-#ifdef HAVE_LIBBSD
-#include <bsd/string.h>
-#endif
 #include <unistd.h>
 #include <errno.h>
 #include <sys/types.h>

ip/xfrm_state.c
@@ -27,9 +27,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#ifdef HAVE_LIBBSD
-#include <bsd/string.h>
-#endif
 #include <netdb.h>
 #include "utils.h"
 #include "xfrm.h"

lib/bpf.c
@@ -15,9 +15,6 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
-#ifdef HAVE_LIBBSD
-#include <bsd/string.h>
-#endif
 #include <stdbool.h>
 #include <stdint.h>
 #include <errno.h>

lib/fs.c
@@ -20,9 +20,6 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
-#ifdef HAVE_LIBBSD
-#include <bsd/string.h>
-#endif
 #include <errno.h>
 #include <limits.h>
 

lib/inet_proto.c
@@ -18,9 +18,6 @@
 #include <netinet/in.h>
 #include <netdb.h>
 #include <string.h>
-#ifdef HAVE_LIBBSD
-#include <bsd/string.h>
-#endif
 
 #include "rt_names.h"
 #include "utils.h"

misc/ss.c
@@ -19,9 +19,6 @@
 #include <sys/sysmacros.h>
 #include <netinet/in.h>
 #include <string.h>
-#ifdef HAVE_LIBBSD
-#include <bsd/string.h>
-#endif
 #include <errno.h>
 #include <netdb.h>
 #include <arpa/inet.h>

tc/em_ipset.c
@@ -20,9 +20,6 @@
 #include <netdb.h>
 #include <unistd.h>
 #include <string.h>
-#ifdef HAVE_LIBBSD
-#include <bsd/string.h>
-#endif
 #include <stdlib.h>
 #include <getopt.h>
 

tc/m_pedit.c
@@ -23,9 +23,6 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <string.h>
-#ifdef HAVE_LIBBSD
-#include <bsd/string.h>
-#endif
 #include <dlfcn.h>
 #include "utils.h"
 #include "tc_util.h"