a6c5c952ab2e0e94eb937b1235649978e22e8125
Guillaume Nault <gnault@redhat.com>
2020-07-01 21:45:04 +0200

    ip link: initial support for bareudp devices
    Bareudp devices provide a generic L3 encapsulation for tunnelling
    different protocols like MPLS, IP, NSH, etc. inside a UDP tunnel.
    
    This patch is based on original work from Martin Varghese:
    https://lore.kernel.org/netdev/1570532361-15163-1-git-send-email-martinvarghesenokia@gmail.com/
    
    Examples:
    
      - ip link add dev bareudp0 type bareudp dstport 6635 ethertype mpls_uc
    
    This creates a bareudp tunnel device which tunnels L3 traffic with
    ethertype 0x8847 (unicast MPLS traffic). The destination port of the
    UDP header will be set to 6635. The device will listen on UDP port 6635
    to receive traffic.
    
      - ip link add dev bareudp0 type bareudp dstport 6635 ethertype ipv4 multiproto
    
    Same as the MPLS example, but for IPv4. The "multiproto" keyword allows
    the device to also tunnel IPv6 traffic.
    
    Signed-off-by: Guillaume Nault <gnault@redhat.com>
    Signed-off-by: Stephen Hemminger <stephen@networkplumber.org>

--- ip/iplink.c
+++ ip/iplink.c
@@ -124,7 +124,7 @@ void iplink_usage(void)
 			"	   bridge | bond | team | ipoib | ip6tnl | ipip | sit | vxlan |\n"
 			"	   gre | gretap | erspan | ip6gre | ip6gretap | ip6erspan |\n"
 			"	   vti | nlmon | team_slave | bond_slave | bridge_slave |\n"
-			"	   ipvlan | ipvtap | geneve | vrf | macsec | netdevsim | rmnet |\n"
+			"	   ipvlan | ipvtap | geneve | bareudp | vrf | macsec | netdevsim | rmnet |\n"
 			"	   xfrm }\n");
 	}
 	exit(-1);
