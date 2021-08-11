iproute2/iproute2

commit 267480f55383cb9f7ddf82412814d3147510df01

Backout the 2.4 utsname hash patch.

tc/f_u32.c
@@ -17,7 +17,6 @@
 #include <syslog.h>
 #include <fcntl.h>
 #include <sys/socket.h>
-#include <sys/utsname.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <string.h>
@@ -877,7 +876,7 @@ static int u32_parse_opt(struct filter_util *qu, char *handle, int argc, char **
 		} else if (strcmp(*argv, "sample") == 0) {
 			__u32 hash;
 			unsigned divisor = 0x100;
-			struct utsname utsname;
+
 			struct {
 				struct tc_u32_sel sel;
 				struct tc_u32_key keys[4];
@@ -902,19 +901,8 @@ static int u32_parse_opt(struct filter_util *qu, char *handle, int argc, char **
 				NEXT_ARG();
 			}
 			hash = sel2.sel.keys[0].val&sel2.sel.keys[0].mask;
-			uname(&utsname);
-			if (strncmp(utsname.release, "2.4.", 4) == 0) {
-				hash ^= hash>>16;
-				hash ^= hash>>8;
-			}
-			else {
-				__u32 mask = sel2.sel.keys[0].mask;
-				while (mask && !(mask & 1)) {
-				  	mask >>= 1;
-					hash >>= 1;
-				}
-				hash &= 0xFF;
-			}
+			hash ^= hash>>16;
+			hash ^= hash>>8;
 			htid = ((hash%divisor)<<12)|(htid&0xFFF00000);
 			sample_ok = 1;
 			continue;

commit 9b7ea92b9e3feff2876f772ace01148b7406839c

tc: u32: Fix key folding in sample option
In between Linux kernel 2.4 and 2.6, key folding for hash tables changed
in kernel space. When iproute2 dropped support for the older algorithm,
the wrong code was removed and kernel 2.4 folding method remained in
place. To get things functional for recent kernels again, restoring the
old code alone was not sufficient - additional byteorder fixes were
needed.

While being at it, make use of ffs() and thereby align the code with how
kernel determines the shift width.

Fixes: 267480f55383c ("Backout the 2.4 utsname hash patch.")
Signed-off-by: Phil Sutter <phil@nwl.cc>
Signed-off-by: Stephen Hemminger <stephen@networkplumber.org>

tc/f_u32.c
@@ -978,6 +978,13 @@ show_k:
 	goto show_k;
 }
 
+static __u32 u32_hash_fold(struct tc_u32_key *key)
+{
+	__u8 fshift = key->mask ? ffs(ntohl(key->mask)) - 1 : 0;
+
+	return ntohl(key->val & key->mask) >> fshift;
+}
+
 static int u32_parse_opt(struct filter_util *qu, char *handle,
 			 int argc, char **argv, struct nlmsghdr *n)
 {
@@ -1110,9 +1117,7 @@ static int u32_parse_opt(struct filter_util *qu, char *handle,
 				}
 				NEXT_ARG();
 			}
-			hash = sel2.keys[0].val & sel2.keys[0].mask;
-			hash ^= hash >> 16;
-			hash ^= hash >> 8;
+			hash = u32_hash_fold(&sel2.keys[0]);
 			htid = ((hash % divisor) << 12) | (htid & 0xFFF00000);
 			sample_ok = 1;
 			continue;