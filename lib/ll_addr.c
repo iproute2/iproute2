e2cc9840eaef3e6676c3e828664f9d0d452248eb
Ralf Baechle <ralf@linux-mips.org>
2021-09-19 15:30:26 +0200

    ROSE: Print decoded addresses rather than hex numbers.
    NETROM is a OSI layer 3 protocol sitting on top of AX.25.  It uses BCD-
    encoded 10 digit telephone numbers as addresses.  Without this ip will
    print a ROSE addresses like
    
      link/rose 12:34:56:78:90 brd 00:00:00:00:00
    
    which is readable but ugly.  With this applied it ROSE addresses will be
    printed as
    
      link/rose 1234567890 brd 0000000000
    
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>
    Signed-off-by: David Ahern <dsahern@kernel.org>

--- lib/ll_addr.c
+++ lib/ll_addr.c
@@ -43,6 +43,8 @@ const char *ll_addr_n2a(const unsigned char *addr, int alen, int type,
 		return ax25_ntop(AF_AX25, addr, buf, blen);
 	if (alen == 7 && type == ARPHRD_NETROM)
 		return netrom_ntop(AF_NETROM, addr, buf, blen);
+	if (alen == 5 && type == ARPHRD_ROSE)
+		return rose_ntop(AF_ROSE, addr, buf, blen);
 
 	snprintf(buf, blen, "%02x", addr[0]);
 	for (i = 1, l = 2; i < alen && l < blen; i++, l += 3)
