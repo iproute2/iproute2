68db921a0a0a96383136900db3c6e7a96fe3b349
Michal Wilczynski <michal.wilczynski@intel.com>
2022-12-01 11:18:10 +0100

    devlink: Fix setting parent for 'rate add'
    Setting a parent during creation of the node doesn't work, despite
    documentation [1] clearly saying that it should.
    
    [1] man/man8/devlink-rate.8
    
    Example:
    $ devlink port function rate add pci/0000:4b:00.0/node_custom parent node_0
      Unknown option "parent"
    
    Fix this by passing DL_OPT_PORT_FN_RATE_PARENT as an argument to
    dl_argv_parse() when it gets called from cmd_port_fn_rate_add().
    
    Fixes: 6c70aca76ef2 ("devlink: Add port func rate support")
    Signed-off-by: Michal Wilczynski <michal.wilczynski@intel.com>
    Reviewed-by: Wojciech Drewek <wojciech.drewek@intel.com>
    Signed-off-by: Stephen Hemminger <stephen@networkplumber.org>

--- devlink/devlink.c
+++ devlink/devlink.c
@@ -5049,7 +5049,8 @@ static int cmd_port_fn_rate_add(struct dl *dl)
 	int err;
 
 	err = dl_argv_parse(dl, DL_OPT_PORT_FN_RATE_NODE_NAME,
-			    DL_OPT_PORT_FN_RATE_TX_SHARE | DL_OPT_PORT_FN_RATE_TX_MAX);
+			    DL_OPT_PORT_FN_RATE_TX_SHARE | DL_OPT_PORT_FN_RATE_TX_MAX |
+			    DL_OPT_PORT_FN_RATE_PARENT);
 	if (err)
 		return err;
 
