1ba8021057acde2ea06f5b52c370f7cc86fb2b3e
Stephen Hemminger <stephen@networkplumber.org>
2023-01-09 13:39:27 -0800

    tc/htb: add SPDX comment
    The standard way is to use SPDX to refer to license,
    instead of per-file boilerplate text.
    
    Signed-off-by: Stephen Hemminger <stephen@networkplumber.org>

--- tc/q_htb.c
+++ tc/q_htb.c
@@ -1,13 +1,7 @@
-/*
- * q_htb.c		HTB.
+/* SPDX-License-Identifier: GPL-2.0 */
+/* q_htb.c	Hierarchical Token Bucket
  *
- *		This program is free software; you can redistribute it and/or
- *		modify it under the terms of the GNU General Public License
- *		as published by the Free Software Foundation; either version
- *		2 of the License, or (at your option) any later version.
- *
- * Authors:	Martin Devera, devik@cdi.cz
- *
+ * Author:	Martin Devera, devik@cdi.cz
  */
 
 #include <stdio.h>
