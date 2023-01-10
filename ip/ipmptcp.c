e2e81aa20f734bada7ca245ad88ff7a6ae6f2630
Matthieu Baerts <matthieu.baerts@tessares.net>
2023-01-10 16:36:20 +0100

    mptcp: add new listener events
    These new events have been added in kernel commit f8c9dfbd875b ("mptcp:
    add pm listener events") by Geliang Tang.
    
    Two new MPTCP Netlink event types for PM listening socket creation and
    closure have been recently added. They will be available in the future
    v6.2 kernel.
    
    They have been added because MPTCP for Linux, when not using the
    in-kernel PM, depends on the userspace PM to create extra listening
    sockets -- called "PM listeners" -- before announcing addresses and
    ports. With the existing MPTCP Netlink events, a userspace PM can create
    PM listeners at startup time, or in response to an incoming connection.
    Creating sockets in response to connections is not optimal: ADD_ADDRs
    can't be sent until the sockets are created and listen()ed, and if all
    connections are closed then it may not be clear to the userspace PM
    daemon that PM listener sockets should be cleaned up. Hence these new
    events: PM listening sockets can be managed based on application
    activity.
    
    Note that the maximum event string size has to be increased by 2 to be
    able to display LISTENER_CREATED without truncated it.
    
    Also, as pointed by Mat, this event doesn't have any "token" attribute
    so this attribute is now printed only if it is available.
    
    Link: https://github.com/multipath-tcp/mptcp_net-next/issues/313
    Cc: Geliang Tang <geliang.tang@suse.com>
    Acked-by: Mat Martineau <mathew.j.martineau@linux.intel.com>
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>
    Signed-off-by: Stephen Hemminger <stephen@networkplumber.org>

--- ip/ipmptcp.c
+++ ip/ipmptcp.c
@@ -454,6 +454,8 @@ static const char * const event_to_str[] = {
 	[MPTCP_EVENT_SUB_ESTABLISHED] = "SF_ESTABLISHED",
 	[MPTCP_EVENT_SUB_CLOSED] = "SF_CLOSED",
 	[MPTCP_EVENT_SUB_PRIORITY] = "SF_PRIO",
+	[MPTCP_EVENT_LISTENER_CREATED] = "LISTENER_CREATED",
+	[MPTCP_EVENT_LISTENER_CLOSED] = "LISTENER_CLOSED",
 };
 
 static void print_addr(const char *key, int af, struct rtattr *value)
@@ -492,11 +494,12 @@ static int mptcp_monitor_msg(struct rtnl_ctrl_data *ctrl,
 		goto out;
 	}
 
-	printf("[%14s]", event_to_str[ghdr->cmd]);
+	printf("[%16s]", event_to_str[ghdr->cmd]);
 
 	parse_rtattr(tb, MPTCP_ATTR_MAX, (void *) ghdr + GENL_HDRLEN, len);
 
-	printf(" token=%08x", rta_getattr_u32(tb[MPTCP_ATTR_TOKEN]));
+	if (tb[MPTCP_ATTR_TOKEN])
+		printf(" token=%08x", rta_getattr_u32(tb[MPTCP_ATTR_TOKEN]));
 
 	if (tb[MPTCP_ATTR_REM_ID])
 		printf(" remid=%u", rta_getattr_u8(tb[MPTCP_ATTR_REM_ID]));
