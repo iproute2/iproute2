c37d21944bcad56c63a27664e4d3ab6d5a98f534
Stephen Hemminger <stephen@networkplumber.org>
2023-01-10 18:53:46 -0800

    ip: use SPDX
    Use SPDX instead of boilerplate text for ip and related
    sub commands.
    
    Signed-off-by: Stephen Hemminger <stephen@networkplumber.org>

--- ip/iplink_bridge_slave.c
+++ ip/iplink_bridge_slave.c
@@ -1,10 +1,6 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
 /*
  * iplink_bridge_slave.c	Bridge slave device support
- *
- *              This program is free software; you can redistribute it and/or
- *              modify it under the terms of the GNU General Public License
- *              as published by the Free Software Foundation; either version
- *              2 of the License, or (at your option) any later version.
  *
  * Authors:     Jiri Pirko <jiri@resnulli.us>
  */
--- ip/ipseg6.c
+++ ip/ipseg6.c
@@ -1,9 +1,6 @@
+/* SPDX-License-Identifier: GPL-2.0 */
 /*
  * seg6.c "ip sr/seg6"
- *
- *	  This program is free software; you can redistribute it and/or
- *	  modify it under the terms of the GNU General Public License
- *	  version 2 as published by the Free Software Foundation;
  *
  * Author: David Lebrun <david.lebrun@uclouvain.be>
  */
