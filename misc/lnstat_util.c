6af63cc7328740fd0c9bf070c5524482daf4230f
Stephen Hemminger <stephen@networkplumber.org>
2023-01-10 19:07:18 -0800

    misc: use SPDX
    Use SPDX tag instead of GPL boilerplate.
    
    Signed-off-by: Stephen Hemminger <stephen@networkplumber.org>

--- misc/lnstat_util.c
+++ misc/lnstat_util.c
@@ -1,3 +1,4 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
 /* lnstat.c:  Unified linux network statistics
  *
  * Copyright (C) 2004 by Harald Welte <laforge@gnumonks.org>
@@ -8,12 +9,6 @@
  *
  * Copyright 2001 by Robert Olsson <robert.olsson@its.uu.se>
  *                                 Uppsala University, Sweden
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
  */
 
 #include <unistd.h>
